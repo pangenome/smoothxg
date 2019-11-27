ecadd566d74387c3ecca6c482bebfc0266db6f25
Adam Novak <anovak@soe.ucsc.edu>
2019-11-27 11:12:59 -0800

    Implement fast edge/base counting

--- src/xg.cpp
+++ src/xg.cpp
@@ -2016,6 +2016,14 @@ size_t XG::get_node_count() const {
     return this->node_count;
 }
 
+size_t XG::get_edge_count() const {
+    return this->edge_count;
+}
+
+size_t XG::get_total_length() const {
+    return this->seq_length;
+}
+
 nid_t XG::min_node_id() const {
     return min_id;
 }
