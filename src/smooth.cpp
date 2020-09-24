f2d7ced9d1a93bc3177948489d285b9fad26f53f
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2020-09-24 18:47:28 +0200

    comparing working and not working abpoa_msa

--- src/smooth.cpp
+++ src/smooth.cpp
@@ -3,11 +3,42 @@
 
 namespace smoothxg {
 
-    char *convert(const std::string & s)
-{
-   char *pc = new char[s.size()+1];
-   std::strcpy(pc, s.c_str());
-   return pc;
+    // do msa for a set of input sequences
+// @function:
+//    generate consensus sequence
+//    generate rc-msa (row column multiple sequence alignment)
+// @para:
+//    ab/abpt: abpoa related variable and parameter
+//    seq_n: number of input sequences
+//    seq_len: array of input sequence length, size: seq_n
+//    seqs: array of input sequences, 0123 for ACGT, size: seq_n * seq_len[]
+int my_abpoa_msa(abpoa_t *ab, abpoa_para_t *abpt, int n_seqs, char **seq_names, int *seq_lens, uint8_t **seqs, FILE *out_fp, uint8_t ***cons_seq, int ***cons_cov, int **cons_l, int *cons_n, uint8_t ***msa_seq, int *msa_l) {
+    if ((!abpt->out_msa && !abpt->out_cons && !abpt->out_gfa) || n_seqs <= 0) return 0;
+    int i, tot_n = n_seqs;
+    uint8_t *is_rc = (uint8_t*)_err_malloc(n_seqs * sizeof(uint8_t));
+    abpoa_reset_graph(ab, abpt, seq_lens[0]);
+    for (i = 0; i < n_seqs; ++i) {
+        abpoa_res_t res;
+        res.graph_cigar = 0, res.n_cigar = 0, res.is_rc = 0;
+        abpoa_align_sequence_to_graph(ab, abpt, seqs[i], seq_lens[i], &res);
+        abpoa_add_graph_alignment(ab, abpt, seqs[i], seq_lens[i], res, i, n_seqs);
+        is_rc[i] = res.is_rc;
+        if (res.n_cigar) free(res.graph_cigar);
+    }
+    if (abpt->out_gfa) {
+        abpoa_generate_gfa(ab, abpt, seq_names, is_rc, n_seqs, out_fp);
+    } else {
+        if (abpt->out_cons) {
+            abpoa_generate_consensus(ab, abpt, tot_n, out_fp, cons_seq, cons_cov, cons_l, cons_n);
+            if (ab->abg->is_called_cons == 0)
+                err_printf("Warning: no consensus sequence generated.\n");
+        }
+        if (abpt->out_msa) {
+            abpoa_generate_rc_msa(ab, abpt, seq_names, is_rc, tot_n, out_fp, msa_seq, msa_l);
+        }
+    }
+    free(is_rc);
+    return 1;
 }
 
     odgi::graph_t smooth_abPOA(const xg::XG& graph,
@@ -82,8 +113,10 @@ namespace smoothxg {
         uint8_t **cons_seq; int **cons_cov, *cons_l, cons_n=0;
         uint8_t **msa_seq; int msa_l=0;
         // perform abpoa-msa
-        // abpoa_msa(ab, abpt, n_seqs, NULL, seq_lens, bseqs, stdout, NULL, NULL, NULL, NULL, NULL, NULL); THIS WORKS!
-        int i, tot_n = n_seqs;
+        abpoa_reset_graph(ab, abpt, seq_lens[0]);
+        abpoa_msa(ab, abpt, n_seqs, NULL, seq_lens, bseqs, stdout, NULL, NULL, NULL, NULL, NULL, NULL); // WORKS
+        my_abpoa_msa(ab, abpt, n_seqs, NULL, seq_lens, bseqs, stdout, NULL, NULL, NULL, NULL, NULL, NULL); // WORKS NOT
+        /* int i, tot_n = n_seqs;
         uint8_t *is_rc = (uint8_t*)_err_malloc(n_seqs * sizeof(uint8_t));
         abpoa_reset_graph(ab, abpt, seq_lens[0]);
         for (i = 0; i < n_seqs; ++i) {
@@ -123,6 +156,7 @@ namespace smoothxg {
             for (i = 0; i < n_seqs; ++i) free(msa_seq[i]);
             free(msa_seq);
         }
+         */
 
 
         std::size_t max_sequence_size = 0;
