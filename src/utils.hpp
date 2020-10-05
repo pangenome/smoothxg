c3f4385acaab7156cb323aca8ebb9edd151e521f
Erik Garrison <erik.garrison@gmail.com>
2020-10-05 14:38:52 +0200

    set parameters to POA in a single CLI argument

---
+++ src/utils.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+
+namespace smoothxg {
+
+template<typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+
+std::vector<std::string> split(const std::string &s, char delim);
+
+}
