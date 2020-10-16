bdd39b16555a667d0312ccfdc02e882fb687ddd0
Erik Garrison <erik.garrison@gmail.com>
2020-10-16 13:35:24 +0200

    graph cleanup function (just sorts)

---
+++ src/cleanup.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <mutex>
+#include "ips4o.hpp"
+#include "odgi/odgi.hpp"
+#include "odgi/chop.hpp"
+#include "odgi/topological_sort.hpp"
+#include "odgi/gfa_to_handle.hpp"
+#include "odgi/dna.hpp"
+#include "odgi/xp.hpp"
+#include "odgi/path_sgd.hpp"
+#include "odgi/threads.hpp"
+#include "odgi/groom.hpp"
+#include "paryfor.hpp"
+
+namespace smoothxg {
+
+// prep the graph into a given GFA file
+// we'll then build the xg index on top of that in low memory
+
+void cleanup(
+    odgi::graph_t graph,
+    const float& p_sgd_min_term_updates,
+    const bool& toposort);
+
+}
