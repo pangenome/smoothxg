0fadad405b9953ba22851634fdba12050e840086
AndreaGuarracino <andreaguarracino@outlook.com>
2020-10-16 09:45:04 +0200

    revert condition to avoid deleting the input when -n is specified

--- src/main.cpp
+++ src/main.cpp
@@ -161,7 +161,7 @@ int main(int argc, char** argv) {
         std::cerr << "[smoothxg::main] building xg index" << std::endl;
         graph.from_gfa(gfa_in_name, args::get(validate),
                        args::get(base).empty() ? gfa_in_name : args::get(base));
-        if (!args::get(keep_temp) || !args::get(no_prep)) {
+        if (!args::get(keep_temp) && !args::get(no_prep)) {
             std::remove(gfa_in_name.c_str());
         }
     }
