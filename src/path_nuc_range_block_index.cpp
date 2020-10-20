fd0f62de22cbf9eed2f2535efef7af34bb0db6c9
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2020-10-20 10:16:23 +0200

    cleanup and concept documentation, block->cons_path mapping still missing

--- src/path_nuc_range_block_index.cpp
+++ src/path_nuc_range_block_index.cpp
@@ -1,17 +1,12 @@
 #include "path_nuc_range_block_index.hpp"
 
+// #define debug_path_nuc_range_block_index
+
 namespace smoothxg {
 
 ska::flat_hash_map<std::string, IITree<uint64_t , uint64_t>> generate_path_nuc_range_block_index(
     std::vector<smoothxg::block_t>& blocks, xg::XG& graph) {
     ska::flat_hash_map<std::string, IITree<uint64_t , uint64_t>> happy_trees_map;
-    /*
-    // TODO PLAY AROUND WITH THE MAP
-    IITree<uint64_t, uint64_t> some_tree = happy_trees_map["a"];
-    if (some_tree.size() == 0) {
-        std::cerr << "SO IT GOES" << std::endl;
-    }
-    */
     for (uint64_t i; i < blocks.size(); i++) {
         auto& block = blocks[i];
         // find out each path's step_handle ranges of a block
@@ -29,16 +24,16 @@ ska::flat_hash_map<std::string, IITree<uint64_t , uint64_t>> generate_path_nuc_r
             } else {
                 happy_tree.add(path_range.nuc_begin, path_range.nuc_end + 1, i);
             }
-            /*
+#ifdef debug_path_nuc_range_block_index
             std::cerr << path_range.nuc_begin << std::endl;
             std::cerr << path_range.nuc_end << std::endl;
-            */
+#endif
         }
     }
     // std::cerr << "happy_trees_map.size() " << happy_trees_map.size() << std::endl;
     for (auto& happy_tree : happy_trees_map) {
         happy_tree.second.index();
-        /*
+#ifdef debug_path_nuc_range_block_index
         std::cerr << "happy_tree.size() " << happy_tree.second.size() << std::endl;
         std::vector<size_t> a;
         IITree tree = happy_tree.second;
@@ -48,24 +43,8 @@ ska::flat_hash_map<std::string, IITree<uint64_t , uint64_t>> generate_path_nuc_r
             std::cerr << tree.end(a[i]) << std::endl;
             std::cerr << tree.data(a[i]) << std::endl;
         }
-        */
+#endif
     }
-    /*
-            IITree<int, int> tree;
-tree.add(12, 34, 0); // add an interval
-tree.add(0, 23, 1);
-tree.add(34, 56, 2);
-tree.index(); // index
-std::vector<size_t> a;
-tree.overlap(23, 25, a); // retrieve overlaps
-for (size_t i = 0; i < a.size(); ++i) {
-    printf("%d\t%d\t%d\n", tree.start(a[i]), tree.end(a[i]), tree.data(a[i]));
-    std::cerr << tree.start(a[i]) << std::endl;
-    std::cerr << tree.end(a[i]) << std::endl;
-    std::cerr << tree.data(a[i]) << std::endl;
-}
-     */
-
     return happy_trees_map;
 }
 
