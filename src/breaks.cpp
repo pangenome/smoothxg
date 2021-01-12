d07ea0bc88d4e04c8a46c995959b0d876c3ddd73
AndreaGuarracino <andreaguarracino@outlook.com>
2021-01-12 14:07:01 +0100

    less operations and a bit strictier max edit distance constraint

--- src/breaks.cpp
+++ src/breaks.cpp
@@ -342,8 +342,10 @@ namespace smoothxg {
                             numeric_limits<uint64_t>::max() :
                             ceil(block_group_identity * (double) curr_fwd.length());
 
+                    double one_minus_block_group_id = 1.0 - block_group_identity;
+
                     // Not for the containment metric
-                    uint64_t max_distance_for_edit_clustering = ceil((1 - block_group_identity) * (double) curr_fwd.length());
+                    uint64_t max_distance_for_edit_clustering = ceil(one_minus_block_group_id * (double) curr_fwd.length());
 
                     uint64_t len_threshold_for_mash_clustering = 0;
                     if (mash_based_clustering_enabled) {
@@ -400,10 +402,9 @@ namespace smoothxg {
                                     EdlibAlignResult result = edlibAlign(
                                             curr.c_str(), curr.size(), other.c_str(), other.size(),
                                             edlibNewAlignConfig(
-                                                    (use_containment_metric ?
-                                                        ceil((1 - block_group_identity) * (double) other.length()) :
-                                                        max_distance_for_edit_clustering
-                                                    ) + 1,
+                                                    use_containment_metric ?
+                                                    ceil(one_minus_block_group_id * (double) other.length()) :
+                                                    max_distance_for_edit_clustering,
                                                     edlib_align_mode,
                                                     EDLIB_TASK_DISTANCE, NULL, 0
                                                     )
