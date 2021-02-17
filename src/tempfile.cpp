fb734b2d9760376e6101ecab2d1a80116b66698f
AndreaGuarracino <andreaguarracino@outlook.com>
2021-02-17 19:13:25 +0100

    use the base directory specified in input also for the path index building before the preparation phase

--- src/tempfile.cpp
+++ src/tempfile.cpp
@@ -96,13 +96,16 @@ namespace temp_file {
 
         // Get the default temp dir from environment variables.
         if (temp_dir.empty()) {
-            const char *system_temp_dir = nullptr;
+            char* cwd = get_current_dir_name();
+            temp_dir = std::string(cwd);
+            free(cwd);
+            /*const char *system_temp_dir = nullptr;
             for (const char *var_name : {"TMPDIR", "TMP", "TEMP", "TEMPDIR", "USERPROFILE"}) {
                 if (system_temp_dir == nullptr) {
                     system_temp_dir = getenv(var_name);
                 }
             }
-            temp_dir = (system_temp_dir == nullptr ? "/tmp" : system_temp_dir);
+            temp_dir = (system_temp_dir == nullptr ? "/tmp" : system_temp_dir);*/
         }
 
         return temp_dir;
