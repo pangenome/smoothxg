02033a1ef656f4ecee07f8441abe7482559f56ed
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2021-06-28 15:50:33 +0200

    replacing dset64-gccAtomic.hpp with dset64.hpp

--- src/blocks.hpp
+++ src/blocks.hpp
@@ -9,7 +9,7 @@
 #include "mmmultimap.hpp"
 #include "xg.hpp"
 #include "flat_hash_map.hpp"
-#include "deps/odgi/src/dset64-gccAtomic.hpp"
+#include "deps/odgi/src/dset64.hpp"
 
 #include "tempfile.hpp"
 
