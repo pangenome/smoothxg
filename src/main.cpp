3cfb7b8205f68607d24a1bf561585716d1a37ba4
Erik Garrison <erik.garrison@gmail.com>
2022-02-02 00:17:57 +0100

    in POA order paths from short to long

--- src/main.cpp
+++ src/main.cpp
@@ -308,7 +308,7 @@ int main(int argc, char **argv) {
 
         }
 
-        const bool order_paths_from_longest = true; //args::get(use_spoa);
+        const bool order_paths_from_longest = false;
         const float term_updates = (_prep_sgd_min_term_updates ? args::get(_prep_sgd_min_term_updates) : 1);
         const int node_chop = (_prep_node_chop ? args::get(_prep_node_chop) : 100);
 
