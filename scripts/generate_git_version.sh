51057e2447d283d04e725323ca9bdb1f29ce0836
AndreaGuarracino <andreaguarracino@outlook.com>
2022-10-27 11:50:58 -0500

    simplify versioning

--- scripts/generate_git_version.sh
+++ scripts/generate_git_version.sh
@@ -2,6 +2,4 @@ INC_DIR=$1
 
 GIT_VERSION=$(git describe --always --tags)
 
-echo "#define SMOOTHXG_GIT_VERSION" \"$GIT_VERSION\" > $1/smoothxg_git_version.hpp.tmp
-diff $1/smoothxg_git_version.hpp.tmp $1/smoothxg_git_version.hpp >/dev/null || cp $1/smoothxg_git_version.hpp.tmp $1/smoothxg_git_version.hpp
-rm -f $1/smoothxg_git_version.hpp.tmp
+echo "#define SMOOTHXG_GIT_VERSION" \"$GIT_VERSION\" > "$INC_DIR"/smoothxg_git_version.hpp
