54a7d1640629db25d1c9e71483eeb3e58a8e7e4e
subwaystation <simon.heumos@qbic.uni-tuebingen.de>
2022-11-10 17:12:57 +0100

    only a happy test makes a happy coder

--- src/main.cpp
+++ src/main.cpp
@@ -360,9 +360,10 @@ int main(int argc, char **argv) {
 
         // It assumes that either xg_in or gfa_in is set
         for (uint64_t current_iter; current_iter < num_iterations; ++current_iter) {
-			uint64_t target_poa_length = std::stoi(target_poa_lengths[current_iter]);
+			uint64_t target_poa_length = (uint64_t)smoothxg::handy_parameter(target_poa_lengths[current_iter], 5000);
 			const uint64_t max_poa_length = _max_poa_length ? (uint64_t)smoothxg::handy_parameter(args::get(_max_poa_length), 2 * target_poa_length) : 2 * target_poa_length;
 			const uint64_t max_block_weight = _max_block_weight ? (uint64_t)smoothxg::handy_parameter(args::get(_max_block_weight), target_poa_length * n_haps) : target_poa_length * n_haps;
+			std::cerr << max_block_weight << std::endl;
 			auto graph = std::make_unique<XG>();
 			const uint64_t current_iter_1_based = current_iter + 1;
 			std::stringstream smoothxg_iter_stream;
