32af616848f7818aa45026a8a10d36620044e410
AndreaGuarracino <andreaguarracino@outlook.com>
2023-08-20 16:08:35 +0200

    fix edge case with 1 path fragment

--- src/main.cpp
+++ src/main.cpp
@@ -680,7 +680,7 @@ int main(int argc, char **argv) {
                             prec_i = i;
                         }
                     }
-                    if (prec_i != path_mapping.size() - 1) {
+                    if (path_mapping.size() == 1 || prec_i != path_mapping.size() - 1) {
                         smoothed->create_path_handle(path_handle_2_name_and_length[prec_path].first);
                         path_handle_2_start_and_end_in_path_mapping[prec_path] = std::make_pair(prec_i, path_mapping.size() - 1);
                     }
